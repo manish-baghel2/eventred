79731bc30f6760c28112751a959fbeaa4007efcb
manish-baghel1 <manish_11914130@nitkkr.ac.in>
2021-10-03 20:22:27 +0530

    updated js file

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+(function() {
+    // animation on scrolling
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
+})()
